30fb198f7783be0829b4017e9357afc0bcbb6fab
sheela Jyothsna <sheela.jyothsna504@gmail.com>
2018-06-26 17:54:13 +0530

    Add files via upload

---
+++ competitive-programming/week-1/Day-1/4.py
@@ -0,0 +1,64 @@
+import unittest
+from operator import itemgetter
+
+import unittest
+from operator import itemgetter
+
+def merge_ranges(meetings):
+    if len(meetings) <= 1:
+        return meetings
+
+    meetings = sorted(meetings, key=itemgetter(0))
+    p = [meetings[0]]
+    for (first, last) in meetings:
+        (start, end) = p[-1]
+        if first <= end:
+            if end <= last:
+                end = last
+            p[-1] = (start, end)
+        else:
+            p.append((first, last))
+    return p
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (2, 4)])
+        expected = [(1, 4)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_touch(self):
+        actual = merge_ranges([(5, 6), (6, 8)])
+        expected = [(5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meeting_contains_other_meeting(self):
+        actual = merge_ranges([(1, 8), (2, 5)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_stay_separate(self):
+        actual = merge_ranges([(1, 3), (4, 8)])
+        expected = [(1, 3), (4, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_multiple_merged_meetings(self):
+        actual = merge_ranges([(1, 4), (2, 5), (5, 8)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_not_sorted(self):
+        actual = merge_ranges([(5, 8), (1, 4), (6, 8)])
+        expected = [(1, 4), (5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_sample_input(self):
+        actual = merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+        expected = [(0, 1), (3, 8), (9, 12)]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
