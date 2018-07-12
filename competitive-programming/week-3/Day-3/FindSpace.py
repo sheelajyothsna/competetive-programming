3758fbafaf6e582f9d02c81c7d4cb492f1503dea
sheela Jyothsna <sheela.jyothsna504@gmail.com>
2018-07-12 09:59:45 +0530

    Update and rename 1.py to FindSpace.py

---
+++ competitive-programming/week-3/Day-3/FindSpace.py
@@ -0,0 +1,44 @@
+import unittest
+
+
+def find_duplicate(arr):
+
+    # Find a number that appears more than once ... in O(n) time
+    size = len(arr)
+     
+  
+    for i in range(0, size):
+         
+        if arr[abs(arr[i])] >= 0:
+            arr[abs(arr[i])] = -arr[abs(arr[i])]
+        else:
+            return abs(arr[i])
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_just_the_repeated_number(self):
+        actual = find_duplicate([1, 1])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_short_list(self):
+        actual = find_duplicate([1, 2, 3, 2])
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_duplicate([1, 2, 5, 5, 5, 5])
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_long_list(self):
+        actual = find_duplicate([4, 1, 4, 8, 3, 2, 7, 6, 5])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
